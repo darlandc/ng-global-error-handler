e9ac1cf9fee90a3069f5fa455cc460f803c91875
Darlan de Castro <darlandc@gmail.com>
2021-05-01 12:41:36 -0300

    starting code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Injector,
+  OnInit,
+  Inject,
+  PLATFORM_ID,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+const configKey = makeStateKey('CONFIG');
+import { TestService } from './services/test.service';
+import * as Sentry from '@sentry/browser';
+declare var webkitSpeechRecognition: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'ng-global-error-handler';
+export class AppComponent implements OnInit {
+  public title: string;
+  public errorText: string;
+  public errBlock: boolean;
+  @ViewChild('gSearch') formSearch;
+  @ViewChild('searchKey') hiddenSearchHandler;
+  constructor(
+    private injector: Injector,
+    public srv: TestService,
+    private state: TransferState,
+    @Inject(PLATFORM_ID) private platformid: object,
+    private renderer: Renderer2
+  ) {
+    this.title = 'Voice Search Demo';
+    if (isPlatformServer(this.platformid)) {
+      const envJson = this.injector.get('CONFIG')
+        ? this.injector.get('CONFIG')
+        : {};
+      this.state.set(configKey, envJson as any);
+    }
+  }
+
+  ngOnInit(): void {
+    this.srv.getData().subscribe(
+      data => {
+        console.log('data returned from server', data);
+      },
+      error => {
+        this.errBlock = true;
+        this.errorText = error.message;
+      }
+    );
+  }
+
+  public voiceSearch() {
+    try {
+      if ('webkitSpeechRecognition' in window) {
+        const vSearch = new webkitSpeechRecognition();
+        vSearch.continuous = false;
+        vSearch.interimresults = false;
+        vSearch.lang = 'en-US';
+        vSearch.start();
+        const voiceSearchForm = this.formSearch.nativeElement;
+        const voiceHandler = this.hiddenSearchHandler.nativeElement;
+        console.log(voiceSearchForm);
+        vSearch.onresult = e => {
+          console.log(voiceSearchForm);
+          voiceHandler.value = e.results[0][0].transcript;
+          vSearch.stop();
+          console.log(voiceHandler);
+          voiceSearchForm.submit();
+        };
+
+        vSearch.onerror = e => {
+          console.log(e);
+          vSearch.stop();
+        };
+      } else {
+        console.log(this.state.get(configKey, undefined as any));
+      }
+    } catch (e) {
+      Sentry.captureException(e);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,74 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler, Injectable, APP_INITIALIZER, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
+import * as Sentry from '@sentry/angular';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpIntercept } from './services/http-interceptor';
+import { Integrations } from '@sentry/tracing';
+import { TestService } from './services/test.service';
+Sentry.init({
+  dsn: 'https://818f3f36b55349c6bd46410cdac2ab2e@o601891.ingest.sentry.io/5744309',
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ['localhost', 'https://yourserver.io/api'],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// setup custom config to capture details about user
+Sentry.configureScope((scope) => {
+  scope.setUser({email : 'darlandc@gmail.com'});
+});
+
+@Injectable()
+// pass angular inbuilt errorhandler to sentry
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error: any): any {
+    Sentry.captureException(error.originalError || error);
+    throw error;
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserTransferStateModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // pass angular inbuilt errorhandler to sentry
+  providers: [
+    TestService,
+    // { provide: ErrorHandler, useClass: SentryErrorHandler },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpIntercept, multi: true},
+      {
+        provide: ErrorHandler,
+        useValue: Sentry.createErrorHandler({
+          showDialog: true,
+        }),
+      },
+      // {
+      //   provide: Sentry.TraceService,
+      //   deps: [Router],
+      // },
+      // {
+      //   provide: APP_INITIALIZER,
+      //   useFactory: () => () => {},
+      //   deps: [Sentry.TraceService],
+      //   multi: true,
+      // },
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
